be876408da58413e33f2bfbe6686c6bfa23e9728
Ronald <kyoberonald@gmail.com>
2020-10-27 21:49:41 +0300

    starting boiler plate for the advanced react sessions

---
+++ src/tutorial/10-prop-types/setup/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Product from './Product';
+import { useFetch } from '../../9-custom-hooks/final/2-useFetch';
+const url = 'https://course-api.netlify.app/api/react-prop-types-example';
+
+const Index = () => {
+  const { products } = useFetch(url);
+  return (
+    <div>
+      <h2>products</h2>
+      <section className='products'>
+        {products.map((product) => {
+          return <Product key={product.id} {...product} />;
+        })}
+      </section>
+    </div>
+  );
+};
+
+export default Index;
