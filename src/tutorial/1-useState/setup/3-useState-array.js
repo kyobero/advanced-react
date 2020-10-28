adf98ca1f77b25c9191824bd8ac21b7eceb5ade9
Ronald <kyoberonald@gmail.com>
2020-10-28 18:51:49 +0300

    using state to delete one item from the array using the id

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -3,6 +3,11 @@ import { data } from '../../../data';
 
 const UseStateArray = () => {
   const [people, setPeople] = React.useState(data);
+
+  const removeItem = (id) => {
+    let newPeople = people.filter((person) => person.id !==id);
+    setPeople(newPeople);
+  };
   return(
      <>
   { people.map((person)=>{
@@ -10,6 +15,7 @@ const UseStateArray = () => {
       return (
         <div key={id} className='item'>
           <h4>{name}</h4>
+          <button onClick={() => removeItem(id)}>remove item</button>
         </div>
       );
     })}
