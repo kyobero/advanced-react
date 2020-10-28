0ec208a7849ae7b2375cc692fdcfdd1c75156d28
Ronald <kyoberonald@gmail.com>
2020-10-28 18:37:40 +0300

    looping through the namez uzing map and rendering the array+clearing everything

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -2,7 +2,20 @@ import React from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = React.useState(data);
+  return(
+     <>
+  { people.map((person)=>{
+      const {id, name } = person;
+      return (
+        <div key={id} className='item'>
+          <h4>{name}</h4>
+        </div>
+      );
+    })}
+    <button className='btn' onClick={() => setPeople([])}>clear All</button>
+    </>
+  );
 };
 
 export default UseStateArray;
