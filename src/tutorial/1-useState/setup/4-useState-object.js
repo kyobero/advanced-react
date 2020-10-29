972f2878967e3af9de7fd1a1a387228b5497e10c
Ronald <kyoberonald@gmail.com>
2020-10-29 16:00:02 +0300

    use state multiple state values

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -7,14 +7,20 @@ const UseStateObject = () => {
     message: 'random message',
   });
 
+  const [name, setName] = useState ('peter');
+  const [age, setAge] = useState (24);
+  const [message, setMessage] = useState ('random message');
+
   const changeMessage = () => {
-    setPerson({...person, message:'hello world'});
+    // setPerson({...person, message:'hello world'});
+    setMessage('hello world');
   };
+
   return( 
   <>
-  <h3>{person.name}</h3>
-  <h3>{person.age}</h3>
-  <h3>{person.message}</h3>
+  <h3>{name}</h3>
+  <h3>{age}</h3>
+  <h3>{message}</h3>
   <button className="btn" onClick={changeMessage}>
     change message
   </button>
