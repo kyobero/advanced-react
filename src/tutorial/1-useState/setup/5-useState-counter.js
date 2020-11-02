d84a38c5c6dba872ea38821169dc5bbdb826eafa
Ronald <kyoberonald@gmail.com>
2020-11-02 18:02:47 +0300

    simple counter with decrease, reset and decrese

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,23 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [value, setValue] = useState(0);
+
+  const reset = () => {
+    setValue(0);
+  };
+
+  return (
+    <>
+      <section style={{ margin: '4rem 0' }}>
+        <h2> regular counter</h2>
+        <h1>{value}</h1>
+        <button className='btn' onClick={() =>setValue(value -1)}>decrease</button>
+        <button className='btn' onClick={reset}>reset</button>
+        <button className='btn' onClick={() => setValue(value + 1)}> increase</button>
+      </section>
+      </>
+      );
 };
 
 export default UseStateCounter;
