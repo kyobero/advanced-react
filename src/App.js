d792229b49971e980af8b9c0ce13bbab7d7cce72
Ronald <kyoberonald@gmail.com>
2020-11-11 13:32:58 +0300

    use effect conditionals. we can not place the hooks inside the conditionals. but can set the contidionals inside the call back method

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Setup from './tutorial/1-useState/setup/5-useState-counter'
+import Setup from './tutorial/2-useEffect/setup/1-useEffect-basics'
 
 function App() {
   return (
--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -2,8 +2,23 @@ import React, { useState, useEffect } from 'react';
 // by default runs after every re-render
 // cleanup function
 // second parameter
-const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+const UseEffectBasics = () => { 
+  const [value,setValue] = useState(0);
+
+  useEffect(()=>{
+    console.log('call useEffect');
+    if (value >= 1) {
+      document.title = 'New Messages(${value})';
+    }
+  });
+
+  console.log('render component');
+  return <>
+  <h1>{value}</h1>
+  <button className="btn" onClick={()=> setValue(value + 1)}>
+    Click me
+  </button>
+  </>;
 };
 
 export default UseEffectBasics;
