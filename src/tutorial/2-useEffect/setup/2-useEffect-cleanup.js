232bd7806656946cc2d488173f895539a193be73
Ronald <kyoberonald@gmail.com>
2020-11-11 20:29:08 +0300

    useEffect clean up after every render

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -4,7 +4,28 @@ import React, { useState, useEffect } from 'react';
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth);
+
+  const checkSize = () => {
+    setSize(window.innerwidth);
+  };
+
+  useEffect(()=>{
+    console.log('useEffect');
+    window.addEventListener('resize', checkSize);
+    return ()=> {
+      console.log ('cleanup');
+      window.removeEventListener('resize', checkSize)
+    }
+  });
+
+
+  return (
+  <>
+  <h1>Window</h1>
+  <h2>{size} PX</h2>
+  </>
+  );
 };
 
 export default UseEffectCleanup;
